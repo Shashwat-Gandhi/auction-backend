a4f3ba667de9f285fe08df090839d8fd88af27bf
Shashwat Gandhi <shashwatgandhi88@gmail.com>
2025-05-15 02:40:17 +0530

    update auction entry

--- src/main/java/com/example/auction/controller/AuctionController.java
+++ src/main/java/com/example/auction/controller/AuctionController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.auction.entities.Auction;
 import com.example.auction.service.AuctionService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/auctions")
@@ -23,9 +27,14 @@ public class AuctionController {
     Logger logger = LoggerFactory.getLogger(AuctionController.class);
 
     @PostMapping("/create")
-    ResponseEntity<Object> createAuction(@RequestBody Auction auction) {
+    public ResponseEntity<Object> createAuction(@RequestBody Auction auction) {
         logger.info("request entity " + auction.getEndTime() + " name: " + auction.getName());
         Auction createdAuction = auctionService.createAuction(auction);
         return new ResponseEntity<Object>("Hi", HttpStatus.CREATED);
+    }
+
+    @PutMapping("auctions/{id}")
+    public Auction putMethodName(@PathVariable Long id, @RequestBody Auction auctionObj) {
+        return auctionService.updateAuction(id, auctionObj);
     }
 }
--- src/main/java/com/example/auction/service/AuctionService.java
+++ src/main/java/com/example/auction/service/AuctionService.java
@@ -1,6 +1,8 @@
 package com.example.auction.service;
 
 
+import javax.persistence.EntityNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +20,16 @@ public class AuctionService {
     public Auction createAuction(Auction newAuction) {
         // validations to be put here, like checking the endTime
         return auctionRepository.save(newAuction);
+    }
+
+    public Auction updateAuction(Long id, Auction auction) {
+        Auction auctionObj = auctionRepository.findById(id).orElseThrow(() ->  new EntityNotFoundException("Auction entity with id:" + id + " not found"));
+        auctionObj.setName(auction.getName());
+        auctionObj.setEndTime(auction.getEndTime());
+        auctionObj.setStartTime(auction.getStartTime());
+        auctionObj.setMinimumBidIncrements(auction.getMinimumBidIncrements());
+        auctionObj.setStartingBid(auction.getStartingBid());
+
+        return auctionRepository.save(auctionObj);
     }
 }
